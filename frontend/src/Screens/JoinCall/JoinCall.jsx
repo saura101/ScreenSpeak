7ee0081e9070da727716abd0cb63f615b49f6eaa
Souradeep <souradeepghosh00@gmail.com>
2023-09-23 16:18:35 +0530

    add join cal to socket

---
+++ frontend/src/Screens/JoinCall/JoinCall.jsx
@@ -0,0 +1,52 @@
+// VideoCall.js
+import React, { useState } from "react";
+import {
+  FaMicrophone,
+  FaMicrophoneSlash,
+  FaPause,
+  FaPlay,
+  FaPhone,
+} from "react-icons/fa";
+import "./JoinCall.css";
+
+const JoinCall = () => {
+  const [isAudioMuted, setIsAudioMuted] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+
+  const toggleAudio = () => {
+    setIsAudioMuted(!isAudioMuted);
+  };
+
+  const togglePause = () => {
+    setIsPaused(!isPaused);
+  };
+
+  const endCall = () => {
+    alert("Call ended");
+  };
+
+  return (
+    <div className="video-call">
+      <div className="video-screen"></div>
+      <div className="controls">
+        <button
+          onClick={toggleAudio}
+          className={`control-button ${isAudioMuted ? "muted" : ""}`}
+        >
+          {isAudioMuted ? <FaMicrophoneSlash /> : <FaMicrophone />}
+        </button>
+        <button onClick={endCall} className="control-button hang-up">
+          <FaPhone />
+        </button>
+        <button
+          onClick={togglePause}
+          className={`control-button ${isPaused ? "paused" : ""}`}
+        >
+          {isPaused ? <FaPlay /> : <FaPause />}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default JoinCall;
