b09e67783ecfc2696b7eacabeafc8b4471ffaad7
Souradeep <souradeepghosh00@gmail.com>
2023-09-24 11:48:40 +0530

    webRTC connection

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,20 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const app=express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
     // options
     cors: {
-        origin: "http://localhost:5173"
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
       }
   });
 
@@ -24,18 +28,25 @@ io.on("connection", (socket) => {
 
     socket.on("join-call",(data) => {
         const { roomID, emailID, name } = data;
-        console.log(`user ${emailID} joined the room ${roomID}`);
+        console.log(`user ${name} and ${emailID} joined the room ${roomID}`);
         emailToSocketMapping.set(emailID,socket.id);
         socketToEmailMapping.set(socket.id,emailID);
         socket.join(roomID);
-        socket.emit("joined-room", {roomID});
+        socket.emit("joined-call", {roomID});
         socket.broadcast.to(roomID).emit("user-joined",{ name,emailID});
     });
-    socket.on("call-user",()=> {
+
+    socket.on("call-user",(data)=> {
         const {emailID, offer } = data;
         const socketID = emailToSocketMapping.get(emailID);
         const fromEmail = socketToEmailMapping.get(socket.id);
         socket.to(socketID).emit("incoming-call", { from : fromEmail , offer});
+    });
+
+    socket.on("call-accepted",(data) => {
+        const { emailID, answer } = data;
+        const socketID = emailToSocketMapping.get(emailID);
+        socket.to(socketID).emit("call-accepted",{ answer })
     });
 
 });
