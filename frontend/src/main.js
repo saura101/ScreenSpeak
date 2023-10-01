e20f890f780f1c18ceb425470538894eeb4aad44
Souradeep <souradeepghosh00@gmail.com>
2023-10-01 23:24:26 +0530

    changed main.jsx to main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { SocketProvider, UserProvider } from "./socket";
+import { SocketProvider, UserProvider } from "./socket.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./Screens/Login/Login.jsx";
 import JoinRoom from "./JoinRoom.jsx";
