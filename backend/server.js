77a8a9ac844578ceafc98490822100821c30b9cf
Souradeep <souradeepghosh00@gmail.com>
2023-10-02 14:33:25 +0530

    added netlify url4

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
     // options
     cors: {
-        origin: "https://extraordinary-crumble-2b10dc.netlify.app/",
+        origin: "https://extraordinary-crumble-2b10dc.netlify.app",
         methods: ["GET", "POST"]
       }
   });
